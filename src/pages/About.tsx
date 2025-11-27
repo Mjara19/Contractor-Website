213e038336c3534a7ee01ca4a5083a0d054d367c
Michael Jara <mikejara19@gmail.com>
2025-11-27 14:13:41 -0500

    Updated UI for mobile

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -18,8 +18,8 @@ const About = () => {
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 1 }}
-              className="text-5xl font-bold mb-6"
-            >ABOUT HMD PRO CONSTRUCTION </motion.h1>
+              className="text-3xl md:text-5xl font-bold mb-6"
+            >ABOUT HMD PRO CONSTRUCTION</motion.h1>
             <motion.div
               initial={{ opacity: 0, y: 30 }}
               animate={{ opacity: 1, y: 0 }}
